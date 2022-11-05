81ad2bbf0fe6f50ae4afc723607b280861639bbe
melomelo7 <65458714+melomelo7@users.noreply.github.com>
2022-11-05 23:39:13 +0900

    Add files via upload

--- arrays.js
+++ arrays.js
@@ -62,7 +62,7 @@ const beasts =
 "fire_hawk","fire_snake","fire_golem","fire_witch",
 "ice_hawk","ice_golem","ice_witch","yeti",
 "ghost","grandma","exorcist","reaper",
-"blood_chicken","blood_rat","blood_spider",
+"blood_chicken","blood_rat","blood_spider","blood_lizard",
 "gem_goblin",
 ]
             
