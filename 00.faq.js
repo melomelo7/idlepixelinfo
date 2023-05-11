e6937ba55eb8716eb3abb76c5b510774f2bedb0f
melomelo7 <65458714+melomelo7@users.noreply.github.com>
2023-05-11 21:25:24 +0900

    Add files via upload

---
+++ 00.faq.js
@@ -0,0 +1,94 @@
+const faqsArray = [
+    {
+    label : "Prestige and Galaxy Value in IPM ?",
+    text : `Every run will(can) end when you reach 10 Millions. next prestige point will need<br>
+            another 2+ Millions. Credits will be received in order to start building rooms<br>
+            on your mothership. The galaxy value when selling is the sum of those : `,
+    url : "",
+    img : "gv.jpg"
+    },
+    {
+    label : "Game Issues",
+    text : `When you encounter a problem with the game that no helper (Discord/Reddit)<br>
+            was able to solve, contact support providing your Player ID (under settings)<br>
+            also check settings > help ... providing screenshots and purchase receipt if needs be.<br><br>
+            Before doing so, a slight issue can be fixed with, depending cases : selling galaxy,<br>
+            closing/force-closing game ... rare case scenario : REBOOT of device !`,
+    url : "support@techtreegames.com",
+    img : ""
+    },
+    {
+    label : "Asteroid | Debris Times",
+    text : `To have these informations it will take a few more clicks :<br>
+            Visit my other tab ⇒ "Projects" and click on projects shown here.<br>
+            (with a purple border around)`,
+    url : "",
+    img : "asteroids times.jpg"
+    },
+]
+
+function setFaq(){
+
+    let thisContainer = document.createElement("div")
+    thisContainer.style = containerRow
+    thisContainer.style.margin = "10px 0 0 20px"
+    right.appendChild(thisContainer)
+
+        let subContainerA = document.createElement("div")
+        subContainerA.style = containerColumn
+        thisContainer.appendChild(subContainerA)
+
+        let subContainerB = document.createElement("div")
+        subContainerB.style = containerColumn
+        thisContainer.appendChild(subContainerB)
+
+    for(i=0;i<faqsArray.length;i++){
+        let btnContainer = document.createElement("div")
+        btnContainer.style = closeButtonStyle
+        btnContainer.style.padding = "10px"
+        subContainerA.appendChild(btnContainer)
+        btnContainer.innerHTML = faqsArray[i].label
+        btnContainer.addEventListener("click",function(e){faqButton(e,subContainerB)})
+    }
+}
+
+function faqButton(e,subContainerB){
+
+    cleanParent(subContainerB)
+    let thisElement = faqsArray[faqsArray.findIndex(x=>x.label === e.srcElement.innerHTML)]
+
+    let thisContainer = document.createElement("div")
+    thisContainer.style = containerStyle
+    thisContainer.style.margin = "10px 0 0 20px"
+
+    subContainerB.appendChild(thisContainer)
+
+        let thisItem = document.createElement("div")
+        thisItem.style = textStyle
+        thisItem.innerHTML = thisElement.text
+        thisContainer.appendChild(thisItem)
+
+        if (thisElement.url){
+            let subContainer = document.createElement("div")
+            thisContainer.appendChild(subContainer)
+            subContainer.style.marginTop = 20 + "px"
+            subContainer.style.textAlign = "center"
+
+                thisItem = document.createElement("a")
+                thisItem.setAttribute("href","mailto:"+thisElement.url)
+                subContainer.appendChild(thisItem)
+                thisItem.innerHTML = thisElement.url
+        }
+
+        if (thisElement.img){
+            subContainer = document.createElement("div")
+            thisContainer.appendChild(subContainer)
+            subContainer.style.marginTop = 20 + "px"
+            subContainer.style.textAlign = "center"
+
+                thisItem = new Image()
+                thisItem.src = "./IPM Components/" + thisElement.img
+                subContainer.appendChild(thisItem)
+        }        
+
+}
--- 7.IPM Index.js
+++ 7.IPM Index.js
@@ -21,6 +21,7 @@ info.innerHTML = infoTitle
 
 let projectPop = undefined
 
+addLeftButton({label:"Faq",clickFunction:clickFaqs})
 addLeftButton({label:"Planets",clickFunction:clickPlanets})
 addLeftButton({label:"Items",clickFunction:clickItems})
 addLeftButton({label:"Projects",clickFunction:clickProjects})
@@ -30,6 +31,38 @@ addLeftButton({label:"Process",clickFunction:clickProcess})
 
 window.onload = firstContact()
 
-console.log("Last up 04/12 00:10")
+console.log("Last up 05/11 21:00")
 
 let loopfork = 0
+
+function myRound(val,multi){
+    let calc = val*multi
+    let res = undefined
+    let calc2 = calc - (calc % 1)
+    if (calc % 1 > 0.5) 
+        {res = 1}
+    else
+        {res = 0}
+
+
+    return calc + " > " + (calc2+res)
+}
+
+/*
+console.log(myRound(5,.9))
+console.log(myRound(3,.9))
+*/
+
+/*
+for (i=0;i<planetsArray.length;i++){
+    let vvv = 0
+    for (j=0;j<planetsArray[i].ores.length;j++){
+        vvv+= planetsArray[i].ores[j].yield
+    }
+
+    if (vvv !== 100) {console.log(planetsArray[i].label + " : " + vvv)}
+    
+
+}
+*/
+
