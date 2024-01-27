300a5ee2bfb007a9b9f8d55bd59b1886feb46342
melomelo7 <65458714+melomelo7@users.noreply.github.com>
2024-01-27 11:14:05 +0900

    Add files via upload

--- 49.PA5864ZY.js
+++ 49.PA5864ZY.js
@@ -1,4 +1,9 @@
 
+function cleanParent(parent){
+    while(parent.children.length >0){
+        parent.removeChild(parent.lastChild)
+    }}
+
 function getID(id){return document.getElementById(id)}
 
 function addEle({
@@ -14,6 +19,7 @@ function addEle({
     setVal = "",
     setFunc = undefined,
     textC = "",
+    textA = "",
     backC = "",
     backG = "",
     height = "",
@@ -66,6 +72,8 @@ function addEle({
         }
 
     if(textC!==""){thisObj.style.color = textC}
+
+    if(textA!==""){thisObj.style.textAlign = textA}
 
     if(backC!==""){thisObj.style.backgroundColor = backC}
 
@@ -127,19 +135,23 @@ let myArr = [
     {label:"月",value:1},
     {label:"影",value:13},
     {label:"日",value:0},
+    {label:"雪",value:16},
     {label:"光",value:9},
     {label:"土",value:6},
     {label:"天",value:15},
     {label:"風",value:11},
+    {label:"銀",value:17},
     {label:"木",value:4},
     {label:"雷",value:8},
-    {label:"雪",value:14},
+    {label:"米",value:14},
+    {label:"野",value:18},
     {label:"火",value:2},
     {label:"石",value:12},
     {label:"水",value:3},
+    {label:"菜",value:19},
     {label:"紙",value:10},
     {label:"金",value:5},
-    {label:"銀",value:7},
+    {label:"升",value:7},
 ]
 
 let refArr = []
@@ -161,7 +173,7 @@ let miss=0
 let radX = 0
 addEle({dad:body,text:"クイズ1：本は？",margin:"10px",setID:"info"})
 addEle({dad:body,setClass:"tab",setID:"cont1"})
-    for(i=0;i<4;i++){
+    for(i=0;i<(myArr.length/4);i++){
         addEle({dad:getID("cont1"),setClass:"contRow",setID:"cont1L"+i})
         for(j=0;j<4;j++){
             radX++
@@ -183,7 +195,7 @@ addEle({dad:body,setClass:"tab",setID:"cont1"})
                                 miss = 0}
                             else 
                                 {miss++ ; x.checked = false
-                                getID("info").innerHTML = "違います...じゃ先教えてね...(ミス："+miss+")"}
+                                getID("info").innerHTML = "違います...じゃ先を教えてね...(ミス："+miss+")"}
                             break
                         case 1 :
                             refV = mdt.getDay() === 0 ? 6 : mdt.getDay() -1
@@ -194,7 +206,7 @@ addEle({dad:body,setClass:"tab",setID:"cont1"})
                                 miss = 0}
                             else 
                                 {miss++ ; x.checked = false
-                                getID("info").innerHTML = "違います...じゃ後教えてね...(ミス："+miss+")"}
+                                getID("info").innerHTML = "違います...じゃ後を教えてね...(ミス："+miss+")"}
                             break
                         case 2 :
                             refV = mdt.getDay() === 6 ? 0 : mdt.getDay() +1
@@ -204,7 +216,7 @@ addEle({dad:body,setClass:"tab",setID:"cont1"})
                                 getID("cont2").style.display = "flex"
                                 miss = 0}
                             else 
-                                {miss++ ; getID("info").innerHTML = "違います...ミス："+miss+")" ; getID("cont1").style.display = "none"}
+                                {miss++ ; getID("info").innerHTML = "違います、クイズ終了...(ミス："+miss+")" ; getID("cont1").style.display = "none"}
                             break                            
                     }
                 }})
@@ -221,7 +233,6 @@ addEle({dad:body,setClass:"tab",setID:"cont1"})
 idx = 0
 radX = 0
 addEle({dad:body,setClass:"tab",setID:"cont2",display:"none",width:"fit-content"})
-//getID("cont2").style.flexWrap = "wrap"
     for(let i=0;i<5;i++){
         addEle({dad:getID("cont2"),setClass:"contRow",setID:"cont2L"+i})
         for(let j=0;j<10;j++){
@@ -233,12 +244,12 @@ addEle({dad:body,setClass:"tab",setID:"cont2",display:"none",width:"fit-content"
                 let mdt = new Date
                 thisGRP.forEach((x)=>{if(x.checked){
                     if(mdt.getDate() === Number(x.value)){
-                        getID("info").style.display = "none"
+                        getID("info").innerHTML = "当たり　！！ クイズ　完成　おめでとう"
                         getID("cont2").style.display = "none"
                         getID("cont3").style.display = "flex"
                     } else {
                         miss++
-                        getID("info").innerHTML = "違います ... ミス："+miss
+                        getID("info").innerHTML = "違います、クイズ終了 ... ミス："+miss
                         if(miss > 2){
                             getID("cont2").style.display = "none"
                         }
@@ -253,5 +264,39 @@ addEle({dad:body,setClass:"tab",setID:"cont2",display:"none",width:"fit-content"
     }
 
 
+let kani = [ {label:myArr[11].label,base:[{val:800,unit:"g"},]} , {label:myArr[15].label,base:[{val:975,unit:"cc"},]} , {label:"ソース",base:[{val:360,unit:"cc"},{val:410,unit:"g"}]} , {label:"ミックス",base:[{val:90,unit:"g"},]} , ]
+addEle({dad:body,setClass:"tab",setID:"cont3",display:"none",width:"fit-content",})
+    addEle({dad:getID("cont3"),setClass:"tab",setID:"cont4",width:"fit-content",})
+        addEle({dad:getID("cont4"),setClass:"clickBtn",setID:"",text:"カニクリームコロッケ",
+        setFunc:()=>{getID("cont5").style.display = getID("cont5").style.display==="none"?"flex":"none";getID("info").style.display="none"}})
+        addEle({dad:getID("cont4"),setClass:"tab",setID:"cont5",display:"none"})
+            addEle({dad:getID("cont5"),setClass:"contRow",setID:"cont5Fork"})
+                addEle({dad:getID("cont5Fork"),setClass:"contCol",setID:"cont5ForkA",})
+                    addEle({dad:getID("cont5ForkA"),setClass:"contRow",setID:"cont5ForkAL1"})
+                        addEle({dad:getID("cont5ForkAL1"),text:0,setID:"kmult",minWidth:"50px",textA:"center"})
+                        addEle({dad:getID("cont5ForkAL1"),text:myArr[19].label,marginL:"10px"})
+                    addEle({dad:getID("cont5ForkA"),setClass:"contRow",setID:"cont5ForkAL2"})
+                        let mw = 40
+                        addEle({dad:getID("cont5ForkAL2"),setClass:"clickBtn",text:"+",minWidth:mw+"px",setFunc:setK})
+                        addEle({dad:getID("cont5ForkAL2"),setClass:"clickBtn",text:"-",minWidth:mw+"px",setFunc:setK})
+                    addEle({dad:getID("cont5ForkA"),setClass:"contRow",setID:"cont5ForkAL3"})
+                        addEle({dad:getID("cont5ForkAL3"),text:"(+/-0.5)",})
+                addEle({dad:getID("cont5Fork"),setClass:"contCol",setID:"cont5ForkB",})
+                    addEle({dad:getID("cont5ForkB"),setClass:"tab",setID:"kcont",display:"none",padding:"10px",marginL:"10px"})
 
-addEle({dad:body,setClass:"tab",setID:"cont3",display:"none",width:"fit-content",text:"当たり　！！ クイズ　完成　おめでとう"})
+function setK(e){
+    let thisL = getID("kmult")
+    let val = Number(thisL.innerHTML)
+    switch(e.srcElement.innerHTML){case "+": val+=0.5 ; break ; case "-": val-=0.5 ; break }
+    if(val < 0.5 || val > 10){thisL.innerHTML = val ; getID("kcont").style.display = "none"}
+    else {
+        thisL.innerHTML = val
+        let cont = getID("kcont")
+        cleanParent(cont)
+        cont.style.display = "flex"
+        kani.forEach((itm)=>{
+            let txt = itm.label + " : "
+            itm.base.forEach((itmB)=>{txt += (itmB.val * (val/0.5))+itmB.unit + " / "})
+            txt = txt.slice(0,txt.length-3)
+            addEle({dad:cont,text:txt})
+        })}}
