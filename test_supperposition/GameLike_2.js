c12d48bb8b47cafef0ffce309ba6294dbf28183e
melomelo7 <65458714+melomelo7@users.noreply.github.com>
2024-02-22 13:15:32 +0900

    GameLike_2.js を更新

--- test_supperposition/GameLike_2.js
+++ test_supperposition/GameLike_2.js
@@ -1,6 +1,6 @@
 const layers = [
 
-{label:"home",backC:"grey",rows:10,cols:14,build:[
+{label:"home",backC:"grey",rows:10,cols:14,builds:[
     {tile:"brick",type:"wall",code:"0x1F9F1",ranges:["1:1~1:14","2:1~9:1","10:1~10:14"]},
 //    {tile:"",type:undefined,code:undefined,cells:[]},
 ]},
