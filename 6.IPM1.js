c51939d445f3826835a30fcaf54ac51b0c665d3a
melomelo7 <65458714+melomelo7@users.noreply.github.com>
2024-08-08 02:21:33 +0900

    Add files via upload

--- 6.IPM1.js
+++ 6.IPM1.js
@@ -250,6 +250,7 @@ function dtSfx(day){
 
 function buildEvent(){
 
+/*    
 txt = `
 Regarding game current bugs, instability, lost of content etc ...<br>
 devs are working on it, until more is know please be patient.<br>
@@ -261,6 +262,7 @@ fontB:"bold", margin:"50px 0 0 50px"})
 
 addEle({dad:right,what:"img",imgFullSrc:"./IPM Components/announcement.jpg",margin:"50px",
 border:"red solid 10px"})
+*/
 
 let greenL = "600px"
 
