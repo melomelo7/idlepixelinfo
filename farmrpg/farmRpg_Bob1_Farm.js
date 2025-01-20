e9069d4a14e64e3431f420cb1228afe6db0c875d
melomelo7 <65458714+melomelo7@users.noreply.github.com>
2025-01-20 19:01:18 +0900

    Add files via upload

--- farmrpg/farmRpg_Bob1_Farm.js
+++ farmrpg/farmRpg_Bob1_Farm.js
@@ -19,7 +19,7 @@ let MCP = 100000
 let orderPool = []
 let savK = "farmRPGOrders"
 
-let shopOpen = true
+let shopOpen = false
 
 let closeTxt = spanText("yellow","Business Hours<br>",22,false,"solid 2px brown")+
 spanText("cyan",`
@@ -38,7 +38,7 @@ let CIM = 100868
 let APM = 75226
 
 
-let lastUp = "01/20 14:55 🇯🇵"
+let lastUp = "01/20 19:00 🇯🇵"
 
 const body = document.querySelector("body")
 
