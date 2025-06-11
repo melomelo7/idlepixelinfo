143eddfdfb191b0bfcd05ef46f56ebf76c43291d
melomelo7 <65458714+melomelo7@users.noreply.github.com>
2025-06-12 07:50:37 +0900

    Update farmRpg_ConvertingTool_2.js

--- farmrpg/farmRpg_ConvertingTool_2.js
+++ farmrpg/farmRpg_ConvertingTool_2.js
@@ -1,6 +1,6 @@
 
 function setPage(){
-    let last = "Last up 2025 06/12 07:40"
+    let last = "Last up 2025 06/12 07:50"
     let from = userI.visuals.preset
 
     let contR = addEle({dad:body,setClass:"contRow",alignItems:"center",margin:"5px"})
