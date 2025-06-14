a1d576e1157ae6f951e888b3d0fde46dc29005f3
melomelo7 <65458714+melomelo7@users.noreply.github.com>
2025-06-14 15:18:29 +0900

    Add files via upload

--- farmrpg/farmpgTestSelect2.js
+++ farmrpg/farmpgTestSelect2.js
@@ -9,6 +9,8 @@ let helpArr = [
     {label:"now showing option 5",text:"option 5"},
 ]
 
+addEle({dad:body,text:"15:17",margin:"10px"})
+
 addEle({dad:body,what:"select",margin:"10px",fontS:"16px",setID:"infosSelect",padding:"5px",
 backC:"darkgreen",border:"rgb(212, 212, 74) solid 2px",radius:"5px",textC:"white",setFunc:()=>{
     getID("result1").innerHTML = "browser is Safari : " + isSafari() + ", " + getID("infosSelect").value
