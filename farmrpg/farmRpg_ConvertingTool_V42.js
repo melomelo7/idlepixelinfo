2bd3019f706568afb6d26889f4d5eff186d93263
melomelo7 <65458714+melomelo7@users.noreply.github.com>
2025-07-28 23:29:39 +0900

    Add files via upload

--- farmrpg/farmRpg_ConvertingTool_V42.js
+++ farmrpg/farmRpg_ConvertingTool_V42.js
@@ -13,7 +13,7 @@ let yellowL = "rgb(212, 212, 74)"
 
 
 let lastUpd = `
-Last up 2025 07/28 22:20
+Last up 2025 07/28 23:30
 <br>`+spanText(green,`
 Users coming from Old version may<br>
 get similar tools by changing <br>
